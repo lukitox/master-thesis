51a038b68c7b263647fa20439310d957be15b1f7
Lukas <l.hi@posteo.de>
2020-11-06 10:49:59 +0100

    Added template for propeller analysis

---
+++ template/template_main.py
@@ -0,0 +1,17 @@
+"""
+Main script of HeLics propeller structural analysis Template
+
+Created on Nov 06 2020
+
+Author: Lukas Hilbers
+"""
+
+# %% Import Libraries and Data
+
+# Third-party imports
+
+
+# Local imports
+
+
+# %%
