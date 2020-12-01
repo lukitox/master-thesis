6c8cb2b256b7673836d272441153d626b057ae4c
Lukas <l.hi@posteo.de>
2020-12-01 11:28:24 +0100

    get_edges() method

--- util_mapdl/prep_functions.py
+++ util_mapdl/prep_functions.py
@@ -1,51 +1,41 @@
-class Material:
+#%% Import Libraries and Data 
+
+# Third-party imports
+import numpy as np
+
+# Local imports
+
+# %%
+
+
+def get_edges(mapdl, y):
     
-    def __init__(self, mapdl, name, number):
-        self.mapdl  = mapdl
-        self.name   = name
-        self.number = number
+    def intersection(lnum):
+        kp_0 =  100000
+        kp_num = 5
+        a_num = 2
         
-        import pathlib
-        self.path = str(pathlib.Path(__file__).parent.absolute())
-    
-    def read_csv(self, csvfile):
-        import csv
-        with open(csvfile, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            MP = {}
-            FC = {}
-            for row in reader:
-                if str(row[0]).upper() == 'MP':
-                    MP[str(row[1])] = float(row[2])
-                    
-                elif str(row[0]).upper() == 'FC':
-                    FC[str(row[1])] = float(row[2]) 
-                
-                else: print('Fehler beim Einlesen, Typ nicht erkannt!')
-                
-        self.mp = MP
-        self.fc = FC
-        # other hinzufügen?
+        mapdl.prep7()
         
-    def save_to_db(self):
-        import json
-        with open('material_'+str(self.name)+'_mp_.json', 'w') as fp:
-            json.dump(self.mp, fp)
-        with open('material_'+str(self.name)+'_fc_.json', 'w') as fp:
-            json.dump(self.fc, fp)            
+        mapdl.k(kp_0+1, -1000, y, -1000)
+        mapdl.k(kp_0+2, 1000, y, -1000)
+        mapdl.k(kp_0+3, 1000, y, 1000)
+        mapdl.k(kp_0+4, -1000, y, 1000)
+        
+        mapdl.a(kp_0+1, kp_0+2, kp_0+3, kp_0+4)
             
-    def load_from_db(self, name):
-        import json
-        with open(self.path+'/material_'+str(self.name)+'_mp_.json', 'r') as fp:
-            self.mp = json.load(fp)
-        with open(self.path+'/material_'+str(self.name)+'_fc_.json', 'r') as fp:
-            self.fc = json.load(fp)
+        mapdl.lina(lnum, a_num)
         
-    def assign_mp(self):
-        for x in self.mp:
-            self.mapdl.mp(str(x),self.number,self.mp[x])
+        coords = []
+        for axis in ['x','y','z']:
+            mapdl.get('c', 'KP', kp_num, 'loc', axis)
+            coords.append(mapdl.parameters['c'])
+        
+        mapdl.kdele(kp_num)
+        
+        return coords
     
-    def assign_fc(self):
-        for x in self.fc:
-            #mapdl.fc('1','s','xten',286)
-            self.mapdl.fc(self.number,'s',str(x),str(self.fc[x]))
+    le = np.array(intersection(4))
+    te = np.array(intersection(2))
+    
+    return le, te, np.linalg.norm(te - le)
