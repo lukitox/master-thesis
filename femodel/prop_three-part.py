5ee9263f97a1fb3a25e12749d108e5cf842ea49b
Lukas <l.hi@posteo.de>
2021-01-12 11:12:32 +0100

    changed change_design_variables() method

--- femodel/prop_three-part.py
+++ femodel/prop_three-part.py
@@ -37,53 +37,58 @@ class Threepartmodel(PropellerModel):
         
         # Read ANSYS Input file (Do not change!)
         self.mapdl.prep7()
-        # self.mapdl.cdread('all', ansys_input_filename, 'cdb')
                 
         # Vary Geometry
         for element in self.element_data['Element Number']:
             self.mapdl.sectype(element,'shell','','')
-            # self.mapdl.secoffset('user',
-            #                      self.element_data['Element offset'][element])
             
             sec = int(self.element_data['Section Number'][element])
             el_height = self.element_data['Element height'][element]
-            core = el_height - args[sec][0]
             
-            if args[sec][0] > el_height:
-                for index in [0,1,5,6]:
-                    self.mapdl.secdata(args[sec][0]/4,
-                                       self.materials['flaxpreg'].number,
-                                       global_vars[index],
-                                       3)
-            else:
-                for index in [0,1]:
-                    self.mapdl.secdata(args[sec][0]/4,
-                                       self.materials['flaxpreg'].number,
-                                       global_vars[index],
-                                       3)
-                self.mapdl.secdata((core
-                                    * args[sec][1] 
-                                    * args[sec][2]),
-                                   self.materials['flaxpreg'].number,
-                                   global_vars[2],
-                                   3)
-                self.mapdl.secdata((core
-                                    * (1 - args[sec][1])),
-                                   self.materials['balsa'].number,
-                                   global_vars[3],
-                                   3)
-                self.mapdl.secdata((core
-                                    * args[sec][1] 
-                                    * (1 - args[sec][2])),
-                                   self.materials['flaxpreg'].number,
-                                   global_vars[4],
-                                   3)
-                for index in [5,6]:
-                    self.mapdl.secdata(args[sec][0]/4,
-                                       self.materials['flaxpreg'].number,
-                                       global_vars[index],
-                                       3)            
+            layer_thickness = 0.185
+            min_thickness = layer_thickness * 2
+            core = el_height - min_thickness
             
-            self.mapdl.emodif(element, 'secnum', element)
+            with self.mapdl.chain_commands:
+                self.mapdl.secdata(min_thickness/2,
+                                  self.materials['flaxpreg'].number,
+                                  global_vars[0],
+                                  3)           
+                
+                if core > 0:
+                    height_balsa = core * (1 - args[sec][0])
+                    
+                    if height_balsa < 1:
+                        for i in range(2):
+                            self.mapdl.secdata(core / 2,
+                                               self.materials['flaxpreg'].number,
+                                               global_vars[0],
+                                               3)
+                    
+                    else:
+                        self.mapdl.secdata((core
+                                            * args[sec][0] 
+                                            * args[sec][1]),
+                                           self.materials['flaxpreg'].number,
+                                           global_vars[0],
+                                           3)                    
+                        self.mapdl.secdata((core
+                                            * (1 - args[sec][0])),
+                                           self.materials['balsa'].number,
+                                           global_vars[1],
+                                           3)      
+                        self.mapdl.secdata((core
+                                            * args[sec][0] 
+                                            * (1 - args[sec][1])),
+                                           self.materials['flaxpreg'].number,
+                                           global_vars[0],
+                                           3)
+                
+                self.mapdl.secdata(min_thickness/2,
+                                  self.materials['flaxpreg'].number,
+                                  global_vars[0],
+                                  3) 
+                
+                self.mapdl.emodif(element, 'secnum', element)
                         
         self.mapdl.allsel('all')
